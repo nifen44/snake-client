99178c54510ba7cd50adc14e71876547787e3758
Wei Huang <huangwei19810803@gmail.com>
2023-05-10 15:28:34 -0600

    modify input.js

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+    connection = conn;
+
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
@@ -14,7 +18,22 @@ const setupInput = function () {
         process.exit();
       }
 
-      process.stdout.write(data);
+    if(data === 'w'){
+        //console.log('Move: up');
+        connection.write('Move: up')
+    }else if(data === 'a'){
+        //console.log('Move: left');
+        connection.write('Move: left')
+    }else if(data === 's'){
+        //console.log('Move: down');      
+        connection.write('Move: down')
+    }else if(data === 'd'){
+        //console.log('Move: right');      
+        connection.write('Move: right')
+    }
+
+      
+    connection.write('Say: hello hello');
   };
 
   module.exports = { setupInput};
